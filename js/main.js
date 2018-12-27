775ec3ab2947bd7ad3629d9b6caf3f04e9f513e7
Sumon Chandra Nath <sumonfbd@gmail.com>
2018-12-27 19:29:09 +0600

    update

--- js/main.js
+++ js/main.js
@@ -39,6 +39,23 @@ $(document).ready(function() {
           transition: function(url){ window.location.href = url; }
         });
 
+      $('.owl-carousel').owlCarousel({
+          loop:true,
+          margin:10,
+          nav:true,
+          responsive:{
+              0:{
+                  items:1
+              },
+              600:{
+                  items:3
+              },
+              1000:{
+                  items:5
+              }
+          }
+      })
+
   });
 
 
